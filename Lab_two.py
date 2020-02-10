648d6ab84d759b05230ca9230c972b9c0f6cc902
Ricardo-Sillas <55039474+Ricardo-Sillas@users.noreply.github.com>
2020-02-10 15:56:57 -0700

    Add files via upload

---
+++ Lab_two.py
@@ -0,0 +1,27 @@
+# Parallel Computing Lab 1
+# Ricardo Sillas
+import pymp
+import time
+from MatrixUtils import genMatrix, genMatrix2, printSubarray
+
+# This is where it multiplies two square matrices where they are of equal size.
+def multiply_matrices(arr):
+    final_arr = [[0 for i in range(len(arr))] for i in range(len(arr))]
+    with pymp.Parallel(6) as p:
+        for i in p.range(len(arr)):
+            for j in p.range(len(arr)):
+                x = 0
+                for k in p.range(len(arr)):
+                    x += (arr[i][k]*arr[k][j])
+                final_arr[i][j] = x
+    return final_arr
+
+def main():
+    array_one = genMatrix()
+    array_two = genMatrix2()
+    start_time = time.time()
+    arr = multiply_matrices(array_two)
+    print((time.time()-start_time))
+    printSubarray(arr)cd
+
+main()
